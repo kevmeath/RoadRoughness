539ad0528e99df0e3b8b64fe1f27eaa27ec6c203
Kevin Meath <kevmeath@gmail.com>
2022-03-10 15:33:48 +0000

    Add function documentation

--- app/src/main/java/km/roadroughness/util/RRMath.java
+++ app/src/main/java/km/roadroughness/util/RRMath.java
@@ -3,6 +3,10 @@ package km.roadroughness.util;
 public class RRMath {
     private RRMath(){};
 
+    /**
+     * @param matrix A 3x3 matrix in the form of a 9 element array
+     * @return Transposed matrix
+     */
     public static float[] transposeMatrix(float[] matrix) {
         float[] transposed = new float[9];
 
@@ -15,6 +19,11 @@ public class RRMath {
         return transposed;
     }
 
+    /**
+     * @param matrixA A 1x3 matrix in the form of a 3 element array
+     * @param matrixB A 3x3 matrix in the form of a 9 element array
+     * @return The product of matrixA and matrixB
+     */
     public static float[] multiplyMatrix(float[] matrixA, float[] matrixB) {
         float[] multiplied = new float[3];
 
@@ -27,10 +36,22 @@ public class RRMath {
         return multiplied;
     }
 
+    /**
+     * @param velocity Initial velocity in meters per second (m/s)
+     * @param acceleration Acceleration in meters per second squared (m/s²)
+     * @param time Time in seconds (s) since initial velocity
+     * @return Final velocity in meters per second (m/s)
+     */
     public static float calcVelocity(float velocity, float acceleration, float time) {
         return velocity + (acceleration * time);
     }
 
+    /**
+     * @param velocity Velocity in meters per second (m/s)
+     * @param acceleration Acceleration in meters per second squared (m/s²)
+     * @param time Time in seconds (s)
+     * @return Distance in meters (m)
+     */
     public static float calcDistance(float velocity, float acceleration, float time) {
         return (velocity * time) + 0.5f * acceleration * (time * time);
     }
