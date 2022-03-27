3158859a7fa029611117311db89e879ef4b98336
Kevin Meath <kevmeath@gmail.com>
2022-03-27 15:04:20 +0100

    Calculate roughness during route recording

--- app/src/main/java/km/roadroughness/RecordRouteActivity.java
+++ app/src/main/java/km/roadroughness/RecordRouteActivity.java
@@ -6,11 +6,18 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Looper;
 import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
@@ -24,27 +31,77 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+import java.util.ArrayList;
+
 import km.roadroughness.databinding.ActivityRecordRouteBinding;
+import km.roadroughness.util.RRMath;
 
-public class RecordRouteActivity extends FragmentActivity implements OnMapReadyCallback {
+public class RecordRouteActivity extends FragmentActivity implements OnMapReadyCallback, SensorEventListener {
 
+    // Map
     private GoogleMap mMap;
     private ActivityRecordRouteBinding binding;
+    private PolylineOptions polylineOptions = new PolylineOptions();
+
+    // Location
     private FusedLocationProviderClient fusedLocationProviderClient;
     private LocationCallback locationCallback = new LocationCallback() {
         @Override
         public void onLocationResult(@NonNull LocationResult locationResult) {
             super.onLocationResult(locationResult);
             for (Location location: locationResult.getLocations()) {
-                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                updateMap(latLng);
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                // Don't record data when stopped.
+                currentSpeed = location.getSpeed();
+                if (currentSpeed > 0) {
+
+                    // If there is at least 1 location recorded, calculate roughness from the last
+                    // location to the current location.
+                    if (route.size() > 0) {
+
+                        // Get distance travelled since the last location
+                        float distance = location.distanceTo(route.get(route.size() - 1));
+
+                        // Calculate roughness vertical distance travelled in meters per horizontal
+                        // distance travelled in kilometers. Add result to a list.
+                        float roughness = verticalDistance / (distance / 1000F);
+                        routeRoughness.add(roughness);
+
+                        // Reset verticalDistance
+                        verticalDistance = 0;
+                    }
+
+                    // Add location to a list.
+                    route.add(location);
+                    updateMap(new LatLng(location.getLatitude(), location.getLongitude()));
+                }
             }
         }
     };
     private final int REQUEST_CODE_ALL_LOCATION = 1;
     private boolean locationPermissionGranted;
-    private PolylineOptions polylineOptions = new PolylineOptions();
+
+    private float currentSpeed = 0;
+
+    // Sensors
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private Sensor rotation;
+    private static final int SENSOR_DELAY = SensorManager.SENSOR_DELAY_UI;
+
+    private float[] accelData = {0,0,0};
+    private float[] accelDataFiltered = {0,0,0};
+    private float[] rotData;
+
+    // Roughness calculation
+    private float verticalDistance = 0;
+    private long previousTime = 0;
+    private float velocity = 0;
+
+    // Route
+    private ArrayList<Location> route = new ArrayList<>();
+    private ArrayList<Float> routeRoughness = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,13 +114,56 @@ public class RecordRouteActivity extends FragmentActivity implements OnMapReadyC
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        // fusedLocationProviderClient for getting location updates
+        // fusedLocationProviderClient for getting location updates.
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
-        // Make sure we have permission to get location updates
+        // Make sure we have permission to get location updates.
         getLocationPermission();
 
-        setupLocationUpdates();
+        // Get sensors necessary for roughness calculation.
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        rotation = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+
+        // Add toggle button to start/stop recording.
+        ToggleButton buttonRecord = (ToggleButton) findViewById(R.id.buttonRecord);
+        buttonRecord.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    start();
+                } else {
+                    stop();
+                }
+            }
+        });
+    }
+
+    /**
+     * Sets up sensor and location listeners which then begin recording data.
+     * Called when the start button is pressed.
+     */
+    private void start() {
+        setupSensorListener();
+
+        if (locationPermissionGranted) {
+            setupLocationUpdates();
+        }
+        else {
+            getLocationPermission();
+        }
+    }
+
+    /**
+     * Terminates sensor and location updates.
+     * Called when the stop button is pressed.
+     */
+    private void stop() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+        sensorManager.unregisterListener(this, accelerometer);
+        sensorManager.unregisterListener(this, rotation);
+
+        Log.d("Route", route.toString());
+        Log.d("Roughness", routeRoughness.toString());
     }
 
     /**
@@ -102,10 +202,21 @@ public class RecordRouteActivity extends FragmentActivity implements OnMapReadyC
         }
     }
 
+    private void setupSensorListener() {
+        // Register sensor listener
+        if (accelerometer != null) {
+            sensorManager.registerListener(this, accelerometer, SENSOR_DELAY);
+        }
+        if (rotation != null) {
+            sensorManager.registerListener(this, rotation, SENSOR_DELAY);
+        }
+    }
+
     private void updateMap(LatLng point) {
         // Add a line connecting to the next point on the route
         polylineOptions.add(point);
         mMap.addPolyline(polylineOptions);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(point));
     }
 
     private void getLocationPermission() {
@@ -136,8 +247,66 @@ public class RecordRouteActivity extends FragmentActivity implements OnMapReadyC
     }
 
     @Override
+    public void onSensorChanged(SensorEvent event) {
+        // Get time interval since last sensor event
+        long currentTime = event.timestamp;
+        if (previousTime == 0) previousTime = event.timestamp;
+        float interval = (currentTime - previousTime) / 1000000000f;
+        previousTime = currentTime;
+
+        if (currentSpeed == 0) return;
+
+        if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+            final float alpha = 0.8f;  // Low-pass filter constant
+
+            // Low-pass filter
+            accelData[0] = alpha * accelData[0] + (1 - alpha) * event.values[0];
+            accelData[1] = alpha * accelData[1] + (1 - alpha) * event.values[1];
+            accelData[2] = alpha * accelData[2] + (1 - alpha) * event.values[2];
+
+            // High-pass filter
+            accelDataFiltered[0] = event.values[0] - accelData[0];
+            accelDataFiltered[1] = event.values[1] - accelData[1];
+            accelDataFiltered[2] = event.values[2] - accelData[2];
+
+        }
+        else if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+            rotData = event.values;
+        }
+
+        float vertAccel = 0;
+
+        if (accelDataFiltered != null && rotData != null) {
+            float[] rotMatrix = new float[9];
+            SensorManager.getRotationMatrixFromVector(rotMatrix, rotData);
+            float[] rotTrans = RRMath.transposeMatrix(rotMatrix);
+            float[] worldAccel = RRMath.multiplyMatrix(accelDataFiltered, rotTrans);
+            vertAccel = worldAccel[2];
+        }
+
+        if (vertAccel >= 0.1f || vertAccel <= -0.1f) {
+            // Vertical velocity of device
+            velocity = Math.abs(RRMath.calcVelocity(velocity, vertAccel, interval));
+
+            // Accumulated vertical distance
+            // This will be used in calculation of roughness
+            verticalDistance += RRMath.calcDistance(velocity, vertAccel, interval);
+        }
+        else {
+            velocity = 0;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
+
+        // Make sure sensor and location updates are stopped before exiting the activity.
         fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+        sensorManager.unregisterListener(this, accelerometer);
+        sensorManager.unregisterListener(this, rotation);
     }
 }
